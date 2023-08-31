320655f6d8e962e11e12e01f4fd18732b105aa15
Mauricio Mercado <mau@maumercado.com>
2023-08-30 18:15:59 -0700

    fix import issues

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import { assert } from 'node:console'
 import { access } from 'node:fs/promises'
-import { fileURLToPath } from 'node:url'
 import dotenv from 'dotenv'
 import fp from 'fastify-plugin'
 import path from 'node:path'
 
-import { matcher } from './lib/secretify'
+import { matcher } from './lib/secretify.js'
 
 const exists = async (file) => {
   try {
@@ -23,9 +22,7 @@ async function resolveSecrets (vars, writeIntoEnv) {
 }
 
 async function env (_, options) {
-  const __filename = fileURLToPath(import.meta.url)
-  const root = path.dirname(__filename)
-
+  const root = process.cwd()
   const possiblePaths = options.path ? path.resolve(path.join(root, options.path, '.env')) : path.resolve(path.join(root, '.env'))
   assert(exists(possiblePaths), 'No .env file found')
 
@@ -33,9 +30,9 @@ async function env (_, options) {
     path: possiblePaths
   })
 
-  await resolveSecrets(vars, process)
+  await resolveSecrets(vars.parsed, process)
 }
 
-export default fp(env, {
-  name: 'env-secrets'
+export default (name = 'fastify-vault-secrets') => fp(env, {
+  name
 })
